d127076c3ecb91f3c2d30ac045b69de4fc3ab684
임성호 <tjd4119@naver.com>
2025-01-23 21:03:16 +0900

    레디스 추가(테스트컨테이너, 커스텀 어노테이션)

---
+++ src/main/java/com/hhplush/eCommerce/infrastructure/redis/DistributedLockAop.java
@@ -0,0 +1,73 @@
+package com.hhplush.eCommerce.infrastructure.redis;
+
+
+import java.lang.reflect.Method;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+@Order(1)
+@RequiredArgsConstructor
+@Slf4j
+public class DistributedLockAop {
+
+    private static final String REDISSON_LOCK_PREFIX = "LOCK:";
+
+    private final RedissonClient redissonClient;
+
+    @Around("@annotation(com.hhplush.eCommerce.infrastructure.redis.IRedissonLock) && args(targetId,..)")
+    public Object aroundLock(ProceedingJoinPoint joinPoint, Long targetId)
+        throws Throwable {
+
+        IRedissonLock redissonLock = getAnnotation(joinPoint);
+
+        String lockName = getLockName(targetId, redissonLock);
+        log.info("lockName: {}", lockName);
+
+        RLock lock = redissonClient.getLock(REDISSON_LOCK_PREFIX + lockName);
+        boolean available = false;
+
+        try {
+            // waitTime 동안 대기하고 leaseTime 동안 락을 유지 ( 분산락 )
+            available = lock.tryLock(redissonLock.waitTime(), redissonLock.leaseTime(),
+                redissonLock.timeUnit());
+            if (!available) {
+                log.warn("Failed to acquire lock: {}", lockName);
+                throw new IllegalArgumentException();
+            }
+            log.info("Successfully acquired lock: {}", lockName);
+
+            // 락 획득 성공 시 비즈니스 로직 수행
+            return joinPoint.proceed();
+
+        } finally {
+            try {
+                lock.unlock();
+            } catch (IllegalMonitorStateException e) {
+                log.error("Failed to unlock the lock: {}", lockName);
+            }
+        }
+    }
+
+    private IRedissonLock getAnnotation(ProceedingJoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        return method.getAnnotation(IRedissonLock.class);
+    }
+
+    private String getLockName(Long targetId, IRedissonLock redissonLock) {
+        String lockNameFormat = "lock:%s:%s";
+        String relevantParameter = targetId.toString();
+        return String.format(lockNameFormat, redissonLock.key(), relevantParameter);
+    }
+}
--- src/test/java/com/hhplush/eCommerce/TestcontainersConfiguration.java
+++ src/test/java/com/hhplush/eCommerce/TestcontainersConfiguration.java
@@ -2,6 +2,7 @@ package com.hhplush.eCommerce;
 
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -9,6 +10,9 @@ import org.testcontainers.utility.DockerImageName;
 class TestcontainersConfiguration {
 
     public static final MySQLContainer<?> MYSQL_CONTAINER;
+
+    // Redis 컨테이너
+    public static final GenericContainer<?> REDIS_CONTAINER;
 
     static {
         MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
@@ -21,12 +25,26 @@ class TestcontainersConfiguration {
             MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
         System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
         System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+
+        // Redis 설정
+        REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse("redis:7.4.2"))
+            .withExposedPorts(6379);  // Redis 기본 포트
+        REDIS_CONTAINER.start();
+
+        // Spring Redis 프로퍼티 설정
+        System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
+        System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
     }
 
     @PreDestroy
     public void preDestroy() {
         if (MYSQL_CONTAINER.isRunning()) {
             MYSQL_CONTAINER.stop();
+        }
+
+        // Redis 컨테이너 종료
+        if (REDIS_CONTAINER.isRunning()) {
+            REDIS_CONTAINER.stop();
         }
     }
 }
