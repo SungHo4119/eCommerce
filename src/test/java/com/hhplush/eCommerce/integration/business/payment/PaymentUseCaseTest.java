f60cd26f7f2e48ce5ba4119118f324057a430a52
임성호 <tjd4119@naver.com>
2025-02-20 23:09:15 +0900

    카프카 발행 / 구독 테스트 코드 작성 실패시 재전송 스케줄러도 구현

--- src/test/java/com/hhplush/eCommerce/integration/business/payment/PaymentUseCaseTest.java
+++ src/test/java/com/hhplush/eCommerce/integration/business/payment/PaymentUseCaseTest.java
@@ -1,7 +1,10 @@
 package com.hhplush.eCommerce.integration.business.payment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import com.hhplush.eCommerce.domain.event.OutboxEvent;
+import com.hhplush.eCommerce.domain.event.ProcessState;
 import com.hhplush.eCommerce.domain.order.Order;
 import com.hhplush.eCommerce.domain.order.OrderState;
 import com.hhplush.eCommerce.domain.payment.Payment;
@@ -9,6 +12,7 @@ import com.hhplush.eCommerce.domain.user.User;
 import com.hhplush.eCommerce.integration.config.IntegrationTest;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -126,6 +130,65 @@ public class PaymentUseCaseTest extends IntegrationTest {
 
             List<Payment> payment = paymentJPARepository.findAll();
             assertEquals(1, payment.size());
+        }
+    }
+
+    @Nested
+    @DisplayName("결재 Kafka 발행")
+    class PaymentConcurrencyKafak {
+
+        @Test
+        void 결재성공시_카프카를통해_메시지를_발행하고_outbox테이블에_저장한다() throws InterruptedException {
+            // given
+            User user = User.builder()
+                .userName("test")
+                .point(10000L)
+                .build();
+
+            user = userJPARepository.save(user);
+
+            Order order = Order.builder().userId(user.getUserId())
+                .orderAmount(1000L)
+                .discountAmount(0L)
+                .paymentAmount(1000L)
+                .orderState(OrderState.PENDING)
+                .orderAt(LocalDateTime.now())
+                .build();
+
+            order = orderJPARepository.save(order);
+
+            // when
+            paymentUseCase.processPaymentWtihPublisher(order.getOrderId());
+
+            // then
+            Thread.sleep(10000);
+            List<OutboxEvent> outboxEvents = outboxEventJPARepository.findAll();
+            assertEquals(1, outboxEvents.size());
+            assertEquals(ProcessState.PROCESSED,
+                outboxEvents.get(0).getProcessState());
+        }
+
+        @Test
+        void 발행실패된메시지가있다면_재전송처리한다() throws InterruptedException {
+
+            // given
+            OutboxEvent outboxEvent = OutboxEvent.builder()
+                .aggregateId("999")
+                .eventType("payment-events")
+                .eventPayload("999")
+                .processState(ProcessState.PUBLISHED_FAILED)
+                .build();
+
+            outboxEvent = outboxEventJPARepository.save(outboxEvent);
+            // then
+            Thread.sleep(10000);
+            Optional<OutboxEvent> result = outboxEventJPARepository.findById(
+                outboxEvent.getOutboxEventId());
+            assertFalse(result.isEmpty());
+            assertEquals(ProcessState.PROCESSED,
+                result.get().getProcessState());
+
+
         }
     }
 }
